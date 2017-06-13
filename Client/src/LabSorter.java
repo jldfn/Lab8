5e49afe30dc4bbfaf0438480e8a5988239036f89
Hououin Kyoma <kjkszpj361@gmail.com>
2017-06-13 14:16:56 +0300

    aljbak

---
+++ Client/src/LabSorter.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import javax.swing.table.TableRowSorter;
+
+/**
+ * Created by Денис on 13.06.2017.
+ */
+public class LabSorter extends RowSorter {
+    @Override
+    public void rowsUpdated(int firstRow, int endRow) {
+
+    }
+}
